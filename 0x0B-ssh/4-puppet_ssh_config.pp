155accf62bc5e7e0148c8ef294643fbc639fbcc4
diego0096 <dfqz93@hotmail.com>
2020-03-24 16:48:36 +0000

    init commit

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#Puppet connect 
+include stdlib
+
+file_line { 'Create_identity_file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Turn_off_passwd_auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
