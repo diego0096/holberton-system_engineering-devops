ad099056af2fce1cb6ed86818c0cd1c74cfcf1db
diego0096 <dfqz93@hotmail.com>
2020-05-04 21:02:47 +0000

    first commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python scipt to run Employee ID"""
+
+if __name__ == "__main__":
+    import csv
+    import json
+    import requests as re
+    from sys import argv
+
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users'
+    user = re.get(url + '/{}'.format(user_id)).json()
+    todo = re.get(url + '/{}/todos'.format(user_id)).json()
+
+    with open("{}.csv".format(user_id), 'w', newline='') as csvfile:
+        fil = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            fil.writerow([user_id,
+                          user.get('username'),
+                          task.get('completed'),
+                          task.get('title')])
