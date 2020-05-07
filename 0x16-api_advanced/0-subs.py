84c397e19ccaaf108d75a69f594f66695aea8f93
diego0096 <dfqz93@hotmail.com>
2020-05-07 23:19:22 +0000

    init commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Contains the number_of_subscribers
 """
 import requests as re
 
 
 def number_of_subscribers(subreddit):
-"""returns the number of subscribers for a given subreddit"""
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     headers = {'User-Agent': 'me'}
