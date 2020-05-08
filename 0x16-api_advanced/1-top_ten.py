11ffe391c64652e37ada36b85108e70ccf6af7be
diego0096 <dfqz93@hotmail.com>
2020-05-08 00:19:30 +0000

    first commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Contains the top ten hot topics
+"""
+
+import requests as re
+
+
+def top_ten(subreddit):
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
+    headers = {'User-Agent': 'me'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+                                    data = re.get(url, headers=headers).json()
+                                        try:
+                                                    children = data.get('data').get('children')
+                                                            for child in children[0:10]:
+                                                                            print(child.get('data').get('title'))
+                                                                                except:
+                                                                                            print(None)
